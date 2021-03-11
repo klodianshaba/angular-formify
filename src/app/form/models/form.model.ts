e3e850a971a5127f7f3c61e1c27be243e94dc004
klodian <dev.klodian@gmail.com>
2021-03-11 11:43:30 +0100

    created FormState

--- src/app/form/models/form.model.ts
+++ src/app/form/models/form.model.ts
@@ -1,14 +1,22 @@
+import {FormGroup} from '@angular/forms';
+
+export interface FormState {
+  controls: boolean;
+  submit?: boolean;
+  options?: boolean;
+}
+
 export  class FormModel{
-  public formGroup: any;
+  public formGroup: FormGroup;
   public loading: boolean;
   public controls: any;
   public submit: any;
   public options: any;
-  constructor(config: any ) {
+  constructor(config: FormState ) {
     this.loading = false;
     this.controls = null;
     this.submit = null;
     this.options = null;
-    this.formGroup = null;
+    this.formGroup = new FormGroup({});
   }
 }
