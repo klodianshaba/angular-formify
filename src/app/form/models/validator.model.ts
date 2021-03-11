ca2cd7612ce9798afdd4ea60f0547b3641072f3f
klodian <dev.klodian@gmail.com>
2021-03-11 19:46:41 +0100

    created ValidatorModel

---
+++ src/app/form/models/validator.model.ts
@@ -0,0 +1,25 @@
+import {ValidatorFn} from '@angular/forms';
+
+export interface ValidatorState {
+  validator?: ValidatorFn;
+  errorCode?: string;
+  description?: string;
+  translationCode?: string;
+  parameters?: any;
+}
+
+export class ValidatorModel {
+  validator: ValidatorFn;
+  errorCode: string;
+  description: string;
+  translationCode: string;
+  parameters: any;
+  constructor( config?: ValidatorState) {
+    this.validator = null;
+    this.errorCode = null;
+    this.description = null;
+    this.translationCode = null;
+    this.parameters = null;
+    Object.assign(this, config);
+  }
+}
