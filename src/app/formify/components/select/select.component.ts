f5fb839ba03f067a80c24a4e2c74819b45e637f8
klodian <dev.klodian@gmail.com>
2021-04-05 21:20:23 +0200

    created select component

---
+++ src/app/formify/components/select/select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'formify-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss']
+})
+export class SelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/formify/formify.module.ts
+++ src/app/formify/formify.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './components/input/input.component';
 import { ControlComponent } from './components/control/control.component';
+import { SelectComponent } from './components/select/select.component';
 
 @NgModule({
-  declarations: [InputComponent, ControlComponent],
+  declarations: [InputComponent, ControlComponent, SelectComponent],
   imports: [
     CommonModule
   ]
