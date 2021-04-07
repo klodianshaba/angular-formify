bb82f8fedccdf7e4ce3ad9e449575e4e79784cc3
klodian <dev.klodian@gmail.com>
2021-04-07 10:06:08 +0200

    create formify component ts

--- src/app/formify/components/formify/formify.component.ts
+++ src/app/formify/components/formify/formify.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, Input, OnChanges, OnInit, SimpleChanges, Output, EventEmitter, ViewChild, ElementRef} from '@angular/core';
+import {ArrayModel, FormModel, GroupModel, SubmitModel} from '../../models/form.model';
+import {FormArray, FormGroup, Validators} from '@angular/forms';
+import { FieldTypes, ControlTypes} from '../../models/formify.model';
+import { FieldModel } from '../../models/field.model';
+import { MatButton } from '@angular/material/button';
 @Component({
   selector: 'formify-formify',
   templateUrl: './formify.component.html',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormifyComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input('form') set onForm(form: FormModel) {this.formModel = form; }
+  @Output('onPrefix') onPrefix: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('submitButton', {static: false}) submitButton: MatButton;
+  public formModel: FormModel;
+  public fieldTypes = FieldTypes;
+  public controlTypes = ControlTypes;
+  get formGroup(): FormGroup {
+    return this.formModel.formGroup;
   }
+  get controls(): (FieldModel | GroupModel | ArrayModel)[] {
+    return this.formModel.controls;
+  }
+  get loading(): boolean {
+    return this.formModel.loading;
+  }
+  get SubmitModel(): SubmitModel {
+    return this.formModel.submit;
+  }
+  constructor() {}
+  ngOnInit() {}
 
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  onSubmit() {}
+
+  onKeyPressAddContactForm(event): void {
+    if ( event.keyCode === 13) {
+      this.submitButton._elementRef.nativeElement.click();
+    }
+  }
+  handlePrefix(event) {
+    this.onPrefix.emit(event);
+  }
 }
