26aa72890e1a6b9850c195db7977d71f09bcd9cc
klodian <dev.klodian@gmail.com>
2021-04-07 16:54:57 +0200

    improved input component

--- src/app/formify/components/input/input.component.ts
+++ src/app/formify/components/input/input.component.ts
@@ -2,7 +2,6 @@ import {AfterContentChecked, AfterContentInit, AfterViewInit, Component, Element
 import {ControlComponent} from '../control/control.component';
 import {ControlValueAccessor, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR, NG_VALIDATORS, AbstractControl} from '@angular/forms';
 import { MatFormFieldAppearance } from '@angular/material/form-field';
-
 @Component({
   selector: 'formify-input',
   templateUrl: './input.component.html',
@@ -11,25 +10,12 @@ import { MatFormFieldAppearance } from '@angular/material/form-field';
     {provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InputComponent) , multi: true}
   ]
 })
-export class InputComponent extends ControlComponent implements ControlValueAccessor,   OnInit , OnChanges, AfterContentInit {
+export class InputComponent extends ControlComponent implements ControlValueAccessor,   OnInit , OnChanges {
   @ViewChild('submit', {static: true}) submit: ElementRef;
   @Output('onPrefix') onPrefix: EventEmitter<boolean> = new EventEmitter<boolean>();
-  // _submitted: boolean = false;
-  // @Input()
-  // public set submitted(value: boolean) {
-  //   this._submitted = value;
-  //   if (value) {
-  //     this.submit.nativeElement.click();
-  //   }
-  // }
-  // public get submitted(): boolean {
-  //   return this._submitted;
-  // }
   onChange: any;
   inputForm: FormGroup;
   constructor(private formBuilder: FormBuilder) {super();}
-  onSubmit(): void {
-  }
   writeValue(obj: any): void {
     this.inputForm.controls['control'].setValue(obj, { emitEvent: false });
   }
@@ -50,13 +36,6 @@ export class InputComponent extends ControlComponent implements ControlValueAcce
   ngOnChanges(changes: SimpleChanges): void {
     // check if there has been any changes on the error and if it is null then clear the filterErrors
     super.ngOnChanges(changes);
-  }
-  ngAfterContentInit(): void {
-    // setTimeout(() => {
-    //   if (this.submitted) {
-    //     this.submit.nativeElement.click();
-    //   }
-    // }, 100);
   }
   ngOnInit(): void {
     this.inputForm = this.formBuilder.group({
