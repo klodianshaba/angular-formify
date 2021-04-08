80e0a3667345d0c3e6f1dbaba7b7d6da424914ab
klodian <dev.klodian@gmail.com>
2021-04-08 12:37:55 +0200

    change control name to dynamic on component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,26 +17,26 @@ export class AppComponent {
           new ValidatorModel( {validator: Validators.required, errorCode: 'required', description: 'name is required'}),
         ]
       }),
-      // new FieldModel({
-      //   controlName: 'language', label: 'Choose your language', multiple: true, placeholder: 'language', autoComplete: 'off', defaultValue: ['en'],  options: [{text: 'Al', value: 'al'}, {text: 'EN', value: 'en'}],
-      //   validators: [
-      //     {validator: Validators.required, errorCode: 'required', description: 'language is required'},
-      //   ]
-      // }),
-      // new FieldModel({
-      //   controlName: 'describe', label: 'Describe your identity', placeholder: 'language', autoComplete: 'off',
-      //   validators: [
-      //     {validator: Validators.required, errorCode: 'required', description: 'identity is required'},
-      //   ]
-      // }),
-      // new FieldModel({controlName: 'color', label: 'Color' , defaultValue: 'primary', options: [{text: 'Primary', value: 'primary'}, {text: 'Warn', value: 'warn'}]}),
-      //
-      // new FieldModel({controlName: 'toggle', label: 'Fill contact', defaultValue: true }),
-      // new FieldModel({controlName: 'checkbox', label: 'check boxes', defaultValue: 'primary',  options: [{text: 'Primary', value: 'primary'}, {text: 'Warn', value: 'warn'}] }),
-      // new ArrayModel('checkboxes', [
-      //   new FieldModel({controlName: 'primary', label: 'Warn'}),
-      //   new FieldModel({controlName: 'warn', label: 'Warn'}),
-      // ]),
+      new FieldModel({
+        controlName: 'language', label: 'Choose your language', multiple: true, placeholder: 'language', autoComplete: 'off', defaultValue: ['en'],  options: [{text: 'Al', value: 'al'}, {text: 'EN', value: 'en'}],
+        validators: [
+          {validator: Validators.required, errorCode: 'required', description: 'language is required'},
+        ]
+      }),
+      new FieldModel({
+        controlName: 'describe', label: 'Describe your identity', placeholder: 'language', autoComplete: 'off',
+        validators: [
+          {validator: Validators.required, errorCode: 'required', description: 'identity is required'},
+        ]
+      }),
+      new FieldModel({controlName: 'color', label: 'Color' , defaultValue: 'primary', options: [{text: 'Primary', value: 'primary'}, {text: 'Warn', value: 'warn'}]}),
+
+      new FieldModel({controlName: 'toggle', label: 'Fill contact', defaultValue: true }),
+      new FieldModel({controlName: 'checkbox', label: 'check boxes', defaultValue: 'primary',  options: [{text: 'Primary', value: 'primary'}, {text: 'Warn', value: 'warn'}] }),
+      new ArrayModel('checkboxes', [
+        new FieldModel({controlName: 'primary', label: 'Warn'}),
+        new FieldModel({controlName: 'warn', label: 'Warn'}),
+      ]),
     ],
     submit: {text: 'Save'}
   });
