30fe620748cfe7e60c5a2a2bdf60b564c6fee6a2
klodian <dev.klodian@gmail.com>
2021-04-09 21:25:45 +0200

    create name field control

---
+++ src/app/formify/fields/password.field-control.ts
@@ -0,0 +1,19 @@
+import {FieldModel, FieldState} from '../models';
+import {Validators} from '@angular/forms';
+
+export class PasswordFieldControl extends FieldModel{
+  constructor(overwrite: FieldState  = {}) {
+    const field: FieldModel = new FieldModel({
+      controlName: 'password',
+      icon: 'vpn_key',
+      type: 'password',
+      label: 'Password',
+      validators: [
+        {validator: Validators.required, errorCode: 'required', description: 'Password is required'},
+        {validator: Validators.minLength(8), errorCode: 'minlength', description: 'Minimum 8 characters are required', parameters: {value: 8}},
+        {validator: null, errorCode: 'invalid' , description: 'Password is invalid'},
+      ]
+    });
+    super(Object.assign(field , overwrite));
+  }
+}
