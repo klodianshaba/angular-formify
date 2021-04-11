a12cbff4fe6bc6759b682c6a509984e3f898711e
klodian <dev.klodian@gmail.com>
2021-04-11 15:34:20 +0200

    create email field control reusable

--- src/app/formify/groups/contact.group-control.ts
+++ src/app/formify/groups/contact.group-control.ts
@@ -1,6 +1,7 @@
 import {FieldModel, FieldState, FieldTypes, GroupModel, GroupState, ValidatorModel} from '../models';
 import {Validators} from '@angular/forms';
 import {Email, Phone} from '../fields';
+import {EmailFieldControl} from '../fields/email.field-control';
 
 export class ContactGroupControl extends GroupModel {
   constructor(overwrite: GroupState  = {}) {
@@ -16,14 +17,7 @@ export class ContactGroupControl extends GroupModel {
             {validator: null, errorCode: 'exist' , description: 'This phone exist in server'},
           ]
         }),
-        new FieldModel({
-          controlName: 'email', label: 'Email address', placeholder: 'email', icon: 'email', autoComplete: 'off',
-          validators: [
-            {validator: Validators.required, errorCode: 'required', description: 'identity is required'},
-            {validator: Validators.pattern(Email) , errorCode: 'pattern' , description: 'Email address is incorrect'},
-            {validator: null, errorCode: 'exist' , description: 'This email exist in server'},
-          ]
-        }),
+        new EmailFieldControl(),
         new FieldModel({
           controlName: 'location', label: 'Enter your location', placeholder: 'location', icon: 'location_on', autoComplete: 'off',
         })
