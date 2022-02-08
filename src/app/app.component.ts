de722d054b3636a2dcf1ce999058715094958f0b
klodian shaba <dev.klodian@gmail.com>
2022-02-08 11:28:09 +0100

    add ngxa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
+import {roll} from 'ngxa';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    roll({direction: 'In'})
+  ]
 })
 export class AppComponent {
   title = 'formify';
